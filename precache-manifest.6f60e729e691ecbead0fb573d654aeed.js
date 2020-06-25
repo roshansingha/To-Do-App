dcd2a78691fb7b7b85bb02b275ae82fff0fd3cf0
roshansingha <roshansinghaagt@gmail.com>
2020-06-25 19:17:10 +0530

    Updates

---
+++ precache-manifest.6f60e729e691ecbead0fb573d654aeed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f530c40c12e7a3414ab537a10321e39",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "790f0ebc627f1d5fd9ee",
+    "url": "/To-Do-App/static/css/main.c4ba5dd9.chunk.css"
+  },
+  {
+    "revision": "0384868ecf1a1e96542f",
+    "url": "/To-Do-App/static/js/2.e2e9aaa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App/static/js/2.e2e9aaa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "790f0ebc627f1d5fd9ee",
+    "url": "/To-Do-App/static/js/main.c3ff180d.chunk.js"
+  },
+  {
+    "revision": "522186ac776c7d0de7c9",
+    "url": "/To-Do-App/static/js/runtime-main.68038795.js"
+  }
+]);
